f0b79169045a88bf614acf79186230921ba73e11
MrNiNaD <ninadparkar273@gmail.com>
2024-08-25 19:20:02 +0530

    feat: env setup

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,12 @@
-import { defineConfig, transformWithEsbuild } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 
+dotenv.config();
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    {
-      name: 'treat-js-files-as-jsx',
-      async transform(code, id) {
-        if (!id.match(/src\/.*\.js$/))  return null
-
-        // Use the exposed transform from vite, instead of directly
-        // transforming with esbuild
-        return transformWithEsbuild(code, id, {
-          loader: 'jsx',
-          jsx: 'automatic',
-        })
-      },
-    },
-    react(),
-  ],
+  plugins: [react()],
   define: {
     'process.env': process.env
-  },
-  optimizeDeps: {
-    force: true,
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
   },
 })
